d6e04e8b0ade1c1a2e43acbfe557377add0168d3
Balazs Toth <bato@itu.dk>
2022-11-23 15:58:22 +0100

    Genetic Algorythm created

---
+++ Assets/Scripts/GA/Individual.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+
+namespace Assets.Scripts
+{
+    public class Individual
+    {
+        int fitness = 0;
+        double [] chromosome = new double[14];
+        int chromosomeLength = 14;
+
+        public Individual()
+        {
+            System.Random rnd = new System.Random();
+
+            for(int j = 0; j < chromosomeLength; j++)
+            {
+                chromosome[j] = rnd.NextDouble();
+            }
+
+            fitness = 0;
+        }
+
+        //Calc fitness
+
+        public void calcFitness()
+        {
+
+            // TODO - it has to come from unity Thymio.getFittness
+            
+
+
+        }
+
+        public int getFitness()
+        {
+            return fitness;
+        }
+
+        public int getChromosomeLength()
+        {
+            return chromosomeLength;
+        }
+
+        public double[] getChromosome()
+        {
+            return chromosome;
+        }
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/GA/Population.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using JetBrains.Annotations;
+using System.IO.IsolatedStorage;
+using static Unity.VisualScripting.LudiqRootObjectEditor;
+using Unity.VisualScripting;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.GA
+{
+    public class Population
+
+
+    {
+        int popSize = 10;
+        int fittestIndex = 0;
+        Individual[] individuals;
+        //Individual[] rouletteList;
+        List<Individual> rouletteList = new List<Individual>();
+        
+
+        public void initPop(int popSize)
+                    
+        {
+            this.popSize = popSize;
+            this.individuals = new Individual[popSize];
+            for (int i = 0; i < individuals.Length; i++)
+            {
+                individuals[i] = new Individual();
+            }
+        }
+
+        //Get the fittest individual within the population
+
+        public Individual getFittest()
+
+        {
+            int maxFit = 0;
+             for( int i = 0; i < individuals.Length; i ++ )
+            {
+                if (maxFit < individuals[i].getFitness())
+                {
+                    maxFit = individuals[i].getFitness();
+                    fittestIndex = i;
+                }
+                
+            }
+             return individuals[fittestIndex];
+
+
+        }
+
+        public int getFittestIndex()
+        {
+            return fittestIndex;
+        }
+
+        //Calculate fitness of each individual
+        public void calculateFitness()
+        {
+
+            for (int i = 0; i < individuals.Length; i++)
+            {
+                individuals[i].calcFitness();
+            }
+            getFittest();
+        }
+
+        void populateRouletteList()
+        {
+            foreach (Individual individual in individuals){
+                
+                for (int j = 0; j < individual.getFitness(); j++)
+                {
+                    rouletteList.Add(individual);
+
+                }
+
+            }
+        }
+
+        public List<Individual> getRouletteList()
+        {
+            return rouletteList;
+        }
+
+        public Individual[] getIndividuals()
+        {
+            return individuals;
+        }
+
+
+
+
+
+    }
+}
