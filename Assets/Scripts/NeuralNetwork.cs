bb5a6a8597ef3aa7e6b0d008a62742425597a7fb
Boris Karavasilev <boka@itu.dk>
2022-11-23 15:48:19 +0100

    Added "MathNet.Numerics" library through Nugget + started a NN

---
+++ Assets/Scripts/NeuralNetwork.cs
@@ -0,0 +1,12 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+
+public class NeuralNetwork
+{
+    private float[] inputLayer;
+    private float[] hiddenLayer;
+    private float[] outputLayer;
+    private Matrix<float> testMatrix; // use matrices to efficiently compute forward propagation of the values
+    
+    // TODO: Implement basic neural network with the "MathNet.Numerics" C# library
+    // see:  https://www.jeremyjordan.me/intro-to-neural-networks/
+}
